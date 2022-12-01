32d451e66f4ed038388aff9084bc254fdaad86dd
Matias <matiasdonato14@gmail.com>
2022-12-01 18:45:06 -0300

    .

---
+++ homeContent/content/src/components/Content.jsx
@@ -0,0 +1,81 @@
+/* eslint-disable jsx-a11y/alt-text */
+import "../css/homeContent.css"
+
+export default function Content(){
+
+
+    return(
+        <div>
+            <center> 
+            <br/> </center>
+            <img src="https://cinni.net/images/site/welcome-heck.png"  className="welcomebanner"/> 
+
+            <img src="https://web.archive.org/web/20091021111606/http://www.geocities.com/bluedevils1us2002/angel2devil.gif" className="welcomeimg imgFloatLeft"/>
+            <div><br/><font color="#ff0000">♡</font> <font color="#e2531c">w</font> <font color="#c69d38">e</font> <font color="#aad655">l</font> <font color="#8df771">c</font> <font color="#71fd8d">o</font> <font color="#55e7aa">m</font> <font color="#38b8c6">e</font> <font color="#1c74e2">♡</font> </div>
+            <p className="pPadding"><span className="spanColor" > ♡ ♡ ♡ <b><i>bienvenidxs</i></b> to my lil corner on the web! soy helena soy re capa soy re programadora ?? mf dont know what an iframe is😂♡ </span></p>
+            <img src="https://cinni.neocities.org/images/web/div/rainbow1.gif"/>
+            <i><u><b>- powered by:</b></u></i><br/><a href="https://neocities.org/" target="_blank" rel="noreferrer"><img src="https://cinni.net/neocities.gif"/></a>
+            <a href="https://anemptyblissbeyondthisworld.neocities.org/" target="_blank" rel="noreferrer" ><img src="https://cinni.net/images/button/leprd.png"/></a>
+            <img src="https://yesterhost.neocities.org/archive/buttons/button102.gif"/>
+            <img src="https://yesterhost.neocities.org/archive/buttons/button142.png"/>
+            <br/>
+            <img src="https://cinni.net/images/web/div/chain02.gif" className="imgPadding2px"/>
+            <span className="spanColor"><b>♡-- . . current status: ★彡･ﾟ✧</b></span><br/>
+            <script src="https://status.cafe/current-status.js?name=cinni"></script>
+            <br/> 
+            <center><img src="https://archive.cinni.net/99gif/items/7/blob_heart.gif"/><br/> 
+            <b>~CINNI NEWS~</b><br/>
+            <span className="spanColor">♡-- . . last update: <script>
+                    document.write(document.lastModified);
+                </script> ★彡･ﾟ✧</span></center>
+                <p className="textAlignLeft">
+                
+                
+                        ♡ light mode is back! and now the halloween theme is the new dark mode~♡<br/>
+
+                <br/>older updates:<br/>
+                    
+                        ♡made a <a href="https://cinni.net/halloween.html" target="_blank" rel="noreferrer">devil-themed silents dollmaker</a> for halloween♡<br/>
+                        ♡joined the <a href="/stickersheet.html">stickersheet club</a>♡<br/>
+                        !-- ♡ new hellscape theme for da spooky season...:3 ♡<br/>
+                        ♡ created a new page my <a href="/theme/neko98.html">neko98 skins</a>~ ^-^ ♡<br/>
+                    ♡ finished chapter 2 of my <a href="https://cinni.net/comic/gemini/chapter2.html" target="_blank" rel="noreferrer">gemini ASCII comic</a>~ ♡<br/>
+                    ♡ created a new page for my <a href="/theme/icons.html">custom desktop icons</a>! ♡<br/>
+                        ♡ finally made a button for my oekaki board :3c ♡<br/>
+                ♡ added new patches to my <a href="https://cinni.net/shrine/furc/patches.html" target="_blank" rel="noreferrer"><b>furc page</b></a>~! ♡<br/>
+                ♡ thanks 2 <a href="https://forum.melonking.net/index.php?topic=115.0" target="_blank" rel="noreferrer"><b>melon's script</b></a> you can now link to pages in the iframe!! ♡ <br/>--
+                !--
+                ♡ i made a smol social network, <a href="https://angel-town.cinni.net/" target="_blank" rel="noreferrer"><b>the angel cafe!</b></a> ♡<br/>
+                ♡ i'm on gemini! check out <a href="gemini://cinni.cities.yesterweb.org/" target="_blank" rel="noreferrer"><b>my capsule!!</b></a> ♡
+            <br/>♡ my bearblog is abandoned no more. <a href="https://cinni.bearblog.dev/" target="_blank" rel="noreferrer"><b>check out my blog~</b></a> ♡
+            <br/>♡ i'm also on mastodon now ^-^ <a href="https://social.yesterweb.org/@cinni" target="_blank" rel="noreferrer"><b>follow meee</b></a> ♡--
+                </p>
+                
+            <img src="https://cinni.net/images/web/div/chain02.gif" className="imgPadding2px"/><br/>
+            <img src="https://cinni.net/images/doll/own/cherry_me.3_blink.gif" className="imgFloatRight"/>
+            <center>
+                <br/><img src="https://cinni.net/images/sym/g1.gif"/><b><i>PLEASE SIGN MY</i> </b>
+            <a href="http://users2.smartgb.com/g/g.php?a=s&i=g26-36688-af" target="_blank" rel="noreferrer"><img src="https://cinni.net/images/guestbook.png"/></a><img src="https://cinni.net/images/sym/g1.gif"/>
+                <br/><br/>
+                <b><i> - check out my other sites? </i></b>♡ ♡ ♡ <br/>
+                <a href="https://oekaki.cinni.net/" target="_blank" rel="noreferrer"><img src="https://cinni.net/images/button/cinni-oekaki-88x31.gif" className="aPadding3px"/></a> <a href="https://archive.cinni.net/" target="_blank" rel="noreferrer"><img src="https://archive.cinni.net/img/cutearchive-88x31.gif" className="aPadding3px"/></a> <a href="https://directory.cinni.net/" target="_blank" rel="noreferrer"><img src="https://cinni.net/directory/img/88x31.png" className="aPadding3px"/></a> <a href="https://99gifshop.neocities.org/" target="_blank" rel="noreferrer"><img src="https://99gifshop.neocities.org/img/88x31.4.png" className="aPadding3px"/></a> <a href="https://entranceorexit.net/" target="_blank" rel="noreferrer"><img src="https://entranceorexit.net/img/eoe_button.png" className="aPadding3px"/></a>
+                </center>
+            <br/><img src="https://cinni.net/images/web/div/chain02.gif" className="imgPadding2px"/>
+            <br/><center><a href="http://tamanotchi.world/1462c"><img src="http://tamanotchi.world/i/1462" alt="It's tamaNOTchi! Click to feed!"/></a>   <a href="http://tamanotchi.world/1605c"><img src="http://tamanotchi.world/i/1605" alt="It's tamaNOTchi! Click to feed!"/></a>
+                        <a href="https://wobble.town/visit/18" target="_blank" rel="noreferrer"><img src="https://wobble.town/visit/18/wobble.gif"/></a><br/><br/><img src="https://cinni.net/images/sym/nice-day.gif"/><br/>
+            <small><i>best viewed on webkit-based browsers (sorry firefox)</i>
+            <br/>
+            <i>p.s. out of the iframe? <a href="https://cinni.neocities.org/"><b>click here</b></a></i></small>
+            </center>
+            
+
+            
+            <br/>
+            <script src="https://99gifshop.neocities.org/theme-control.js"></script>
+            <script data-goatcounter="https://cinni.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>
+            </div>
+            
+    )
+}
+
+
