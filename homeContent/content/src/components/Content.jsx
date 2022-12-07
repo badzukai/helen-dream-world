b86b8906d2dabf7e179d8d02bf69855e735daf1b
badzukai <helutrindade@gmail.com>
2022-12-06 21:25:09 -0300

    cambié todo

--- homeContent/content/src/components/Content.jsx
+++ homeContent/content/src/components/Content.jsx
@@ -1,8 +1,14 @@
 /* eslint-disable jsx-a11y/alt-text */
 import "../css/homeContent.css"
 
-export default function Content(){
 
+
+export default function Content(){
+    let date = new Date()
+    let dayDate = date.toLocaleDateString();
+    let hourDate = date.toString().split(" ")[4]
+
+    console.log(date);
 
     return(
         <div>
@@ -12,57 +18,80 @@ export default function Content(){
 
             <img src="http://dl3.glitter-graphics.net/pub/344/344663ev9dicoavn.gif" className="welcomeimg imgFloatLeft"/>
         <div className="welcometml">
-            <p className="pPadding"><span className="spanColor" > ♡ ♡ ♡ <b><i>bienvenidas</i></b> to my lil corner on the web! soy helena ♡ ♡ ♡ </span></p> 
+            <p className="pPadding">
+                
+                <span className="spanColorbien" > ♡ ♡ ♡ <b><i>bienvenidas</i></b> to my lil corner on the web! soy helena ♡ ♡ ♡ </span></p> 
             </div>
-            
-            
-            <br/>
-            <img src="https://cinni.net/images/web/div/chain02.gif" className="imgPadding2px"/>
-            <span className="spanColor"><b>♡-- . . current status: butakera ★彡･ﾟ✧</b></span><br/>
-            <script src="https://status.cafe/current-status.js?name=cinni"></script>
-            <br/> 
-            <center><img src="https://archive.cinni.net/99gif/items/7/blob_heart.gif"/><br/> 
-            <b>~HELEN NEWS~</b><br/>
-            <span className="spanColor">♡-- . . last update: <script>
-                    document.write(document.lastModified);
-                </script> ★彡･ﾟ✧</span></center>
-                <p className="textAlignLeft">
+             <img src="https://archive.cinni.net/99gif/items/7/blob_heart.gif"/><br/> 
+           
+        
+        <div className="h2">
+            <b>
+                <span className= "textocomun">
+            ✎ . . current status ★彡･ﾟ✧ 
+                </span>
+             </b> 
+        </div>
+            {/* {document.Write(document.LastModified)} */}
+                
+        <div className="h2">
+            <b>
+                <span className= "textocomun">
+            {`✎ . . last update: ${dayDate} - ${hourDate}  ★彡･ﾟ✧ `}
+                </span>
+             </b> 
+        </div>
+
+        <div className="body">
+                <p className="lastUpdates">
                 
                 
-                        ♡ konoyaro♡<br/>
+                        ♡ currently working on : <br />
+                        ♡ blog <br />
+                        ♡ photodump <br />
+                        ♡ links <br/>
 
                 <br/>older updates:<br/>
                     
-                        ♡pelotudeces q vaya haciendo <a href="https://cinni.net/halloween.html" target="_blank" rel="noreferrer">a</a> ♡<br/>
-                        ♡a <a href="/stickersheet.html">stickersheet club</a>♡<br/>
-                        !-- ♡ dejo los links para q me quede guardado y desp cambio ♡<br/>
-                        ♡ ca <a href="/theme/neko98.html">neko98 skins</a>~ ^-^ ♡<br/>
-                    ♡ a <a href="https://cinni.net/comic/gemini/chapter2.html" target="_blank" rel="noreferrer">gemini </a>~ ♡<br/>
-                    ♡a <a href="/theme/icons.html">custom desktop icons</a>! ♡<br/>
-                        ♡ a ♡<br/>
-                ♡ a <a href="https://cinni.net/shrine/furc/patches.html" target="_blank" rel="noreferrer"><b></b></a>~! ♡<br/>
-                ♡ a <a href="https://forum.melonking.net/index.php?topic=115.0" target="_blank" rel="noreferrer"><b>t</b></a> !! ♡ <br/>--
-                !--
-                ♡ a <a href="https://angel-town.cinni.net/" target="_blank" rel="noreferrer"><b>!</b></a> ♡<br/>
-                ♡ a <a href="gemini://cinni.cities.yesterweb.org/" target="_blank" rel="noreferrer"><b>!!</b></a> ♡
-            <br/>♡ a <a href="https://cinni.bearblog.dev/" target="_blank" rel="noreferrer"><b>c~</b></a> ♡
-            <br/>♡ a <a href="https://social.yesterweb.org/@cinni" target="_blank" rel="noreferrer"><b>f</b></a> ♡--
+                        ♡ <br /> 
+                        ♡ <br /> 
+                        ♡ <br /> 
+                        ♡ <br /> 
+                        ♡ <br /> 
+                        ♡ <br /> 
+                        ♡ <br /> 
+                        ♡ <br /> 
+
+           
                 </p>
-                
-            <img src="https://cinni.net/images/web/div/chain02.gif" className="imgPadding2px"/><br/>
-            <img src="https://web.archive.org/web/20191101011527im_/http://www.easydoll.com/mooddolls/mooddoll9.gif" className="imgFloatRight"/>
+        </div>
+            <br />
+            <br />
+        
+       
+                <img src="https://web.archive.org/web/20191101011527im_/http://www.easydoll.com/mooddolls/mooddoll9.gif" className="imgFloatRight"/>
             <center>
+                <br />
+                <p className="dividertop"> 
+                ┌──❀*̥˚──◌──◌──❀*̥˚─┐
+                </p>
                 <div className="guestbookContainer">
                     <br/>
+                    
                     <img src="https://cinni.net/images/sym/g1.gif"/><b><i> firmen mi </i> </b>
                     <a href="http://users3.smartgb.com/g/g.php?a=s&i=g36-34086-6c" target="_blank" rel="noreferrer"><img src="https://cinni.net/images/guestbook.png"/></a><img src="https://cinni.net/images/sym/g1.gif"/>
-                    <br/>
-                </div>
-                
-                </center>
-            <br/><img src="https://cinni.net/images/web/div/chain02.gif" className="imgPadding2px"/>
-            <br/><center> <img src="https://web.archive.org/web/20190911145822im_/http://www.easydoll.com/dividers/div9.gif"  />
-                        <img src="https://cinni.net/images/sym/nice-day.gif"/><br/>
+                   <br />
+                </div>     
+                <p className="dividerbottom"> 
+                └◌───❀*̥˚───◌───❀*̥˚┘
+                </p>          
+            </center>
+                <br />
+                <br />
+            <center> 
+                <img src="https://web.archive.org/web/20190911145822im_/http://www.easydoll.com/dividers/div9.gif"  />
+                <img src="https://cinni.net/images/sym/nice-day.gif"/>
+                <br/>
             
             </center>
             
